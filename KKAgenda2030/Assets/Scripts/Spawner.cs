0d6cdf319cee8d21f7a44183f285e4f033d01a2c
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2018-10-25 11:35:43 +0300

    Maanpäällinen elämä Proto vol.0.1

---
+++ KKAgenda2030/Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] rubbish;                
+    public float spawnTime = 1f;            
+    public Transform[] spawnPoints;         
+        
+    // Use this for initialization
+    void Start ()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {        
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int rubbishObjectsIndex = Random.Range(0, rubbish.Length);
+               
+        Instantiate(rubbish[rubbishObjectsIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);      
+
+    }
+}
+
+
---
+++ KKAgenda2030/Assets/Scripts/TrashDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TrashDestroy: MonoBehaviour {
+    public List<TrashType> acceptTypes;
+          
+
+    private void OnTriggerEnter(Collider other)
+    {      
+        if (acceptTypes.Contains(other.GetComponent<Trash>().kind))
+        {   
+            Destroy(other.gameObject);
+
+            print(other.gameObject.tag);
+        }
+        
+    }
+}
