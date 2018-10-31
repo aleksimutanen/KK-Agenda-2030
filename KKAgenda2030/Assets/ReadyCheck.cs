e3c6268dcfe3cd8a4a5b90952d171ce71d518c19
DESKTOP-BLSHGE6\bergj <berg.joni.petteri@gmail.com>
2018-10-31 21:55:31 +0200

    Ready Checks codes.

---
+++ KKAgenda2030/Assets/ReadyCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadyCheck : MonoBehaviour {
+
+    public bool[] isReady;
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- KKAgenda2030/Assets/Scripts/DeletingTrash.cs
+++ KKAgenda2030/Assets/Scripts/DeletingTrash.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class DeletingTrash : MonoBehaviour
 {
     public List<TrashType> disqTypes;
-
+    
     private void OnTriggerEnter(Collider other)
     {
         
         if (disqTypes.Contains(other.GetComponent<Trash>().kind))
         {
+            other.GetComponent<Spawner>().rubbish.Add(gameObject);
             Destroy(other.gameObject);
             TrashGameManager.instance.DeletingPoints();
             TrashGameManager.instance.UpdatePoints();
             
         }
-
 
     }
 
