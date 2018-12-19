a76fa208a601cc28c2791c302cca3ec8f8953dba
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-12-19 10:29:00 +0200

    pageturning for menu

--- KKAgenda2030/Assets/Scripts/PageTurner.cs
+++ KKAgenda2030/Assets/Scripts/PageTurner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum Pages { Cover, Sea, Earth };
 
@@ -10,7 +11,11 @@ public class PageTurner : MonoBehaviour {
 
     public GameObject[] pages;
 
-    int pageIndex;
+    public Button[] buttons;
+    public GameObject nextPageButton;
+    public GameObject previousPageButton;
+
+    public int pageIndex;
 
     void Start () {
         page = Pages.Cover;
@@ -25,17 +30,44 @@ public class PageTurner : MonoBehaviour {
     }
 
     public void PreviousPage() {
+        StartCoroutine("PreviousPageAnim");
+    }
+
+   
+    IEnumerator PreviousPageAnim() {
         if (pageIndex >= 1) {
+            if (pageIndex == pages.Length - 1) nextPageButton.SetActive(true);
+            foreach (Button b in buttons) b.interactable = false;
+            pages[pageIndex - 1].SetActive(true);
             pages[pageIndex - 1].GetComponent<Animator>().Play("PageBack");
+
+            yield return new WaitForSeconds(0.5f);
+
+            pages[pageIndex].SetActive(false);
             pageIndex--;
+            foreach (Button b in buttons) b.interactable = true;
+            if (pageIndex == 0) previousPageButton.SetActive(false);
             page = Pages.Cover + pageIndex;
         }
     }
 
     public void NextPage() {
+        StartCoroutine("NextPageAnim");
+    }
+
+    IEnumerator NextPageAnim() {
         if (pageIndex < pages.Length) {
+            if (pageIndex == 0) previousPageButton.SetActive(true);
+            foreach (Button b in buttons) b.interactable = false;
+            pages[pageIndex + 1].SetActive(true);
             pages[pageIndex].GetComponent<Animator>().Play("Page");
+
+            yield return new WaitForSeconds(0.5f);
+
+            pages[pageIndex].SetActive(false);
             pageIndex++;
+            foreach (Button b in buttons) b.interactable = true;
+            if (pageIndex == pages.Length - 1) nextPageButton.SetActive(false);
             page = Pages.Cover + pageIndex;
         }
     }
