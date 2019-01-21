3cc5a831b5a3f0070b145e6affecbd3730d55460
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2019-01-21 14:43:29 +0200

    Lelujen korjaus Scenen alkua.

---
+++ KKAgenda2030/Assets/Scripts/ToyRepair.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyRepair : MonoBehaviour
+{ 
+    public List<GameObject> partOfToy;
+
+    private void OnCollisionEnter(Collision col)  
+    {
+        if (col.gameObject.tag == "RepairCube")
+        {
+            print("Nyt osuu!");
+
+            partOfToy.Add(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
