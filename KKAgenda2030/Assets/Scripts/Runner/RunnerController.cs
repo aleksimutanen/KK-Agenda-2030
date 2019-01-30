1e703054a748720027f77504f12f750f6ee6e985
aleksimutanen <mutanen.aleksi@hotmail.com>
2019-01-30 23:31:45 +0200

    few tests

--- KKAgenda2030/Assets/Scripts/Runner/RunnerController.cs
+++ KKAgenda2030/Assets/Scripts/Runner/RunnerController.cs
@@ -15,6 +15,8 @@ public class RunnerController : MonoBehaviour {
 
     Rigidbody rb;
 
+    public GameObject map;
+
     void Start() {
         rb = GetComponent<Rigidbody>();
     }
@@ -64,31 +66,45 @@ public class RunnerController : MonoBehaviour {
     void Update() {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 newDir = Vector3.zero;
 
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, background)) {
-            if (Input.GetKey(KeyCode.Mouse0)) {
-                speedFactor += Time.deltaTime;
-                speedFactor = Mathf.Clamp01(speedFactor);
-            } else {
-                speedFactor -= Time.deltaTime;
-                speedFactor = Mathf.Clamp01(speedFactor);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, background) && Input.GetKey(KeyCode.Mouse0)) {
+            speedFactor += Time.deltaTime;
+            speedFactor = Mathf.Clamp01(speedFactor);
+
+            if (hit.point.z > transform.position.z) {
+                rb.velocity = transform.forward * speedFactor;
+            } else if (hit.point.z < transform.position.z) {
+                rb.velocity = -transform.forward * speedFactor;
+            }
+        } else {
+            speedFactor -= Time.deltaTime;
+            speedFactor = Mathf.Clamp01(speedFactor);
+
+            if (rb.velocity.z > 0) {
+                rb.velocity = transform.forward * speedFactor;
+            } else if (rb.velocity.z < 0) {
+                rb.velocity = -transform.forward * speedFactor;
             }
         }
 
-        bool dir = rb.velocity.z > 0 ?
-               up = true :
-               up = false;
+        map.transform.position += -transform.right * speed * Time.deltaTime;
 
-        if (hit.point.z > transform.position.z)
-            rb.velocity = transform.forward * speedFactor;
-        else if (hit.point.z < transform.position.z)
-            rb.velocity = -transform.forward * speedFactor;
+        //bool dir = rb.velocity.z > 0 ?
+        //       up = true :
+        //       up = false;
+
+        
+
+        //newDir.x = transform.right.x * speed;
 
         //rb.velocity = transform.right * speed;
+        //rb.velocity = newDir;
 
-        Vector3 camPos = transform.position;
-        camPos.y += 10f;
-        Camera.main.transform.position = camPos;
+        //Vector3 camPos = transform.position;
+        //camPos.y += 10f;
+        //Camera.main.transform.position = camPos;
+
 
         //sprintFactor = sprintT > sprintDuration ?
         //0f :
