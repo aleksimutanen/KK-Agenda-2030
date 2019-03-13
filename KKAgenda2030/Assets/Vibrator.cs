6308f44a0b0d9152a7e5bd11845ca384c00f293a
Nikl3 <niko.tuuli@hotmail.com>
2019-03-13 16:13:13 +0200

    sin vibratoor to Kiira

---
+++ KKAgenda2030/Assets/Vibrator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vibrator : MonoBehaviour
+{
+    public float amplitude;
+    public float frequency;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.localPosition = Vector3.up * Mathf.Sin(2 * Mathf.PI / (1 / frequency) * Time.time) * amplitude;
+
+
+    }
+}
