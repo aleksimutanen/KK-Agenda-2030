5bd49a16b8d6e41a8a7151496ebe83896b1a49fd
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2019-03-14 10:47:20 +0200

    World Decal Game

--- KKAgenda2030/Assets/Scripts/WorldGame/WorldManager.cs
+++ KKAgenda2030/Assets/Scripts/WorldGame/WorldManager.cs
@@ -1,41 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WorldManager : MonoBehaviour
 {
-    public GameObject decalsFolder;
+    public GameObject decalsFolder;   
 
-    public List<GameObject> decals;
-    public Transform[] decPositions;
-   
+    public GameObject[] images = new GameObject[8];
 
 
-    void Start()
-    {
-
-        for (int i = 0; i < decals.Count + 5;)
+    void Update()
         {
 
-            decalsSpawn();
-            i++;
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                    print(hit.point);
+                
+                    var nGo = new GameObject("temp");
+
+                    GameObject go = Instantiate(nGo, hit.point, Quaternion.identity);
+                    print("Luodaan uusi image");
+                    go.transform.parent = decalsFolder.transform;
+                    go.gameObject.AddComponent<Image>();
+                    // go.gameObject.GetComponent<Image>().sprite = hit.collider.;
+                    go.gameObject.AddComponent<ItemDragHandler>();
+                    go.layer = 5;
+                    // hit.collider.gameObject.SetActive(false);
+                   go.transform.position = (Input.mousePosition);
+
+
+                }
+            }
         }
-
-
-    }
-
-    public void decalsSpawn()
-    {
-        
-          
-            var rnd = Random.Range(0, decals.Count);
-            var pos = Random.Range(0, decPositions.Length);
-
-         
-
-            var decs = Instantiate(decals[rnd], decPositions[pos].position, Quaternion.identity);
-            decs.transform.parent = decalsFolder.transform;
-            decals.RemoveAt(rnd);
-          
-    }
 }
