55ea29b74377a1ec532c2b7e20e31eeb173d4d75
Nikl3 <niko.tuuli@hotmail.com>
2019-03-28 15:52:59 +0200

    some camera capture testing to memorygame

--- KKAgenda2030/Assets/SceneSwitch.cs
+++ KKAgenda2030/Assets/SceneSwitch.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour {
-    
+
+    public string sceneName;
+
     public void LoadToMenu() {
         SceneManager.LoadScene(0);
+    }
+
+    public void SwitchScene() {
+        SceneManager.LoadScene(sceneName);
     }
 }
---
+++ KKAgenda2030/Assets/Scripts/MemoryGame/PhoneCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneCamera : MonoBehaviour {
+
+    bool camAvailable;
+    WebCamTexture frontCam;
+    Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+
+    void Start() {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0) {
+            Debug.Log("No camera detected");
+            camAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++) {
+            if (devices[i].isFrontFacing) {
+                frontCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+        if (frontCam == null) {
+            Debug.Log("Unable to find back camera");
+            return;
+        }
+
+        frontCam.Play();
+        background.texture = frontCam;
+        camAvailable = true;
+    }
+
+    void Update() {
+        if (!camAvailable) {
+            return;
+        }
+        float ratio = (float)frontCam.width / (float)frontCam.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY = frontCam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -frontCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+}
