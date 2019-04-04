02d2176cc9f9c4749f42199022cac2a8df78d017
Nikl3 <niko.tuuli@hotmail.com>
2019-04-04 15:37:41 +0300

    Memory game camera thing and some interactions

--- KKAgenda2030/Assets/Scripts/MemoryGame/AvatarClickBehaviour.cs
+++ KKAgenda2030/Assets/Scripts/MemoryGame/AvatarClickBehaviour.cs
@@ -6,18 +6,35 @@ public class AvatarClickBehaviour : MonoBehaviour {
 
     MemoryGameManager mgm;
     public int thisPicIndx;
+    Animator animator;
 
     void Start() {
         mgm = FindObjectOfType<MemoryGameManager>();
+        animator = GetComponent<Animator>();
     }
 
 
+    public void OnSelected() {
+        animator.Play("AvatarGlow");
+    }
+
+    public void OnDeselected() {
+        animator.Play("New State");
+    }
+
     public void ClickAvatar() {
-        print("image hit");
-        var currentPicIndx = mgm.pictureIndx;
-        if (currentPicIndx != thisPicIndx) {
-            mgm.pictureIndx = thisPicIndx;
-            // toggle some effect?
-        }
+        mgm.SlotSelected(thisPicIndx);
+
+        //for (int i = 0; i < playerAvatars.Count; i++) {
+        //    var acb = FindObjectOfType<AvatarClickBehaviour>().currentlySelected = false;
+        //}
+        //var currentPicIndx = mgm.pictureIndx;
+        //if (currentPicIndx != thisPicIndx) {
+        //    mgm.pictureIndx = thisPicIndx;
+        //    currentlySelected = true;
+        //    // toggle some effect?
+        //    print("image hit, " + "pictureIndx is now " + thisPicIndx);
+
+        //}
     }
 }
--- KKAgenda2030/Assets/Scripts/MemoryGame/InsertPlayers.cs
+++ KKAgenda2030/Assets/Scripts/MemoryGame/InsertPlayers.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class InsertPlayers : MonoBehaviour {
 
     MemoryGameManager mgm;
+    public GameObject avatarPanel;
+    public GameObject player1Avatar;
+    public GameObject player2Avatar;
+
 
     void Start() {
         mgm = FindObjectOfType<MemoryGameManager>();
     }
 
     public void Insert1Player() {
-        // 
+        mgm.playerCount = 1;
+        gameObject.SetActive(false);
+        avatarPanel.SetActive(true);
+        player2Avatar.SetActive(false);
     }
 
     public void Insert2Player() {
-        // 
+        mgm.playerCount = 2;
+        gameObject.SetActive(false);
+        avatarPanel.SetActive(true);
     }
 
 }
