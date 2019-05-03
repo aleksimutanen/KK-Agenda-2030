a0acc80181868333bd29fb7f3619a84cb469551c
aleksimutanen <mutanen.aleksi@hotmail.com>
2019-05-03 13:25:50 +0300

    Started energygame

---
+++ KKAgenda2030/Assets/Scripts/EnergyGame/ButtonManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+    public enum scenes { livingRoom, Yard, Kitchen }
+
+    public scenes currentScene;
+
+    public int sceneIndex;
+
+    public List<Toggle> livingRoomToggles;
+    public List<Image> livingRoomOnImages;
+    public List<Image> livingRoomOffImages;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void changeImage() {
+        if (currentScene == scenes.livingRoom) {
+            foreach(Toggle toggle in livingRoomToggles) {
+
+            }
+        }
+    }
+}
---
+++ KKAgenda2030/Assets/Scripts/EnergyGame/ImageToggler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageToggler : MonoBehaviour
+{
+    public List<Image> images;
+    public int selectedImage;
+
+    public void changeImage() {
+        images[selectedImage].gameObject.SetActive(false);
+        selectedImage = 1 - selectedImage;
+        images[selectedImage].gameObject.SetActive(true);
+    }
+}
