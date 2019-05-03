7092b0f57b222d180053e07567a6a6b0c3d469f0
aleksimutanen <mutanen.aleksi@hotmail.com>
2019-05-03 15:03:46 +0300

    started gamelogic on how to change scenes in the game (energygame)

--- KKAgenda2030/Assets/Scripts/EnergyGame/ButtonManager.cs
+++ KKAgenda2030/Assets/Scripts/EnergyGame/ButtonManager.cs
@@ -4,30 +4,58 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour {
-    public enum scenes { livingRoom, Yard, Kitchen }
+    public enum Scenes { livingRoom, Yard, Kitchen }
 
-    public scenes currentScene;
+    public Scenes currentScene;
 
     public int sceneIndex;
+    public List<SceneParent> sceneFolder;
+
+    //public List<Scene> listScenes;
+    //public List<Toggle> toggles;
 
     public List<Toggle> livingRoomToggles;
-    public List<Image> livingRoomOnImages;
-    public List<Image> livingRoomOffImages;
+    public List<Toggle> yardToggles;
+    public List<Toggle> kitchenToggles;
 
     void Start() {
-
     }
 
-    // Update is called once per frame
     void Update() {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) NextScene();
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) PreviousScene();
 
     }
 
-    public void changeImage() {
-        if (currentScene == scenes.livingRoom) {
-            foreach(Toggle toggle in livingRoomToggles) {
-
-            }
+    public void NextScene() {
+        foreach (Toggle toggle in sceneFolder[sceneIndex].toggles) {
+            toggle.interactable = false;
         }
+        //transition something
+
+        sceneFolder[sceneIndex].gameObject.SetActive(false);
+        sceneFolder[sceneIndex + 1].gameObject.SetActive(true);
+
+        foreach (Toggle toggle in sceneFolder[sceneIndex + 1].toggles) {
+            toggle.interactable = true;
+        }
+
+        sceneIndex++;
+    }
+
+    public void PreviousScene() {
+        foreach (Toggle toggle in sceneFolder[sceneIndex].toggles) {
+            toggle.interactable = false;
+        }
+        //transition something
+
+        sceneFolder[sceneIndex].gameObject.SetActive(false);
+        sceneFolder[sceneIndex - 1].gameObject.SetActive(true);
+
+        foreach (Toggle toggle in sceneFolder[sceneIndex - 1].toggles) {
+            toggle.interactable = true;
+        }
+
+        sceneIndex--;
     }
 }
