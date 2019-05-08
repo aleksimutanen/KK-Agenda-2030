2262e3cb9eee13b76b5dc251a48945c32a6dd92a
Nikl3 <niko.tuuli@hotmail.com>
2019-05-08 16:04:16 +0300

    memorygame menu page draggings, logic now working

---
+++ KKAgenda2030/Assets/AnimalDrag_Kaarlo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalDrag_Kaarlo : MonoBehaviour {
+
+    Vector3 snapPos;
+    public MenuKaarlo_drag mkd;
+    public string snapPosName;
+    //public GameObject parentFolder;
+
+
+    public void Start() {
+        snapPos = GameObject.Find(snapPosName).transform.position;
+        //transform.parent = parentFolder.transform;
+
+    }
+
+    void Update() {
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0f;
+        transform.position = worldPos;
+
+        if (Input.GetKeyUp(KeyCode.Mouse0)) { // TODO: phoneTouch
+            OnEndDrag();
+        }
+    }
+
+    public void OnEndDrag() {
+        print("onmouseup NOW");
+        var colliders = Physics.OverlapSphere(transform.position, 1f);
+        bool found = false;
+        foreach (var item in colliders) {
+            if (item.tag == "Decal")
+                found = true;
+        }
+        if (found) {
+            print("decal hit");
+            transform.position = snapPos;
+            this.enabled = false;
+        } else {
+            gameObject.SetActive(false);
+            mkd.ResetState();
+        }
+    }
+}
---
+++ KKAgenda2030/Assets/Scripts/Menu/MenuKaarlo_drag.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuKaarlo_drag : MonoBehaviour {
+
+    public Sprite dragSprite;
+    public GameObject draggableObject;
+    GameObject tempPrefab;
+    Animator animator;
+    public string emptyAnimation;
+    public string defaultAnimation;
+    
+    public Sprite jigsawHalo;
+
+    Vector3 dist;
+    float posX;
+    float posY;
+    bool dragging = false;
+    ScrollRect sLock;
+    GameObject kaarloScrollRect;
+
+    Vector3 startPos;
+    
+
+    //new animation when drag start, instantiate or toggle on draggable object. If dropped outside goal, switch back.
+
+
+    void Start() {
+        kaarloScrollRect = GameObject.Find("Page5_Kaarlo");
+        sLock = kaarloScrollRect.GetComponent<ScrollRect>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void ResetState() {
+        this.enabled = true;
+        animator.Play(defaultAnimation);
+    }
+
+    private void OnMouseDown() {
+        if (!enabled) {
+            return;
+        }
+        sLock.vertical = !enabled;
+        animator.Play(emptyAnimation);
+        var dragAnimal = Instantiate(draggableObject, transform.position, Quaternion.identity);
+        dragAnimal.GetComponent<AnimalDrag_Kaarlo>().mkd = this;
+        this.enabled = false;
+
+        //startPos = transform.position;
+        //dragging = true;
+        //dist = Camera.main.WorldToScreenPoint(transform.position);
+        //posX = Input.mousePosition.x - dist.x;
+        //posY = Input.mousePosition.y - dist.y;
+    }
+
+    //public void OnMouseDrag() {
+    //    Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, dist.z);
+    //    Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+    //    transform.position = worldPos;
+    //}
+
+    //public void OnMouseUp() {
+    //    var colliders = Physics.OverlapSphere(transform.position, 1f);
+    //    var dragToys = new List<GameObject>();
+    //    foreach (var item in colliders) {
+    //        if (item.tag == "Halo") {
+    //            dragToys.Add(item.gameObject);
+    //        }
+    //    }
+    //    if (dragToys.Count == 0) {
+    //        transform.position = startPos;
+    //        animator.Play(defaultAnimation);
+    //        // play default animation
+    //    } else {
+    //        transform.position = snapPos;
+    //        // this items collider off, so item couldnt be moved anymore?
+    //    }
+    //    sLock.vertical = enabled;
+    //    dragging = false;
+    //}
+
+}
