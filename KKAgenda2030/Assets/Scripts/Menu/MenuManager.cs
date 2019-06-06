0804a84135e79596a6e97f1c00c081455c601ac4
Nikl3 <niko.tuuli@hotmail.com>
2019-06-06 15:32:57 +0300

    music button particles when music is playing

--- KKAgenda2030/Assets/Scripts/Menu/MenuManager.cs
+++ KKAgenda2030/Assets/Scripts/Menu/MenuManager.cs
@@ -10,7 +10,6 @@ public class MenuManager : MonoBehaviour {
     public GameObject MenuForThegame;
     public GameObject videoScreen;
     public GameObject creditsTheGame;
-    private VideoPlayer videoPlayer;
     public VideoPlayer vp;
     [HideInInspector] public AudioSource audioData;
     private string Web =  "https://";
@@ -20,15 +19,14 @@ public class MenuManager : MonoBehaviour {
     public VideoClip[] videoClips = new VideoClip[6];
     public VideoClip[] DIYClips = new VideoClip[6];
     public GameObject[] audioSourceGO = new GameObject[6];
+    public GameObject[] musicButtonParticles = new GameObject[6];
 
 
     void Awake() {
         pt = GameObject.Find("PageTurner").GetComponent<PageTurner>();
         grandManager = GameObject.Find("GrandManager").GetComponent<GrandManager>();
-        videoPlayer = videoScreen.GetComponent<VideoPlayer>();
         audioData = GetComponent<AudioSource>();
         creditsTheGame.gameObject.SetActive(false);
-        videoScreen.gameObject.SetActive(false);
     }
 
     private void Update() {
@@ -38,6 +36,8 @@ public class MenuManager : MonoBehaviour {
     }
 
     public void TestPlay(VideoClip clip) {
+        StopMusic();
+
         vp.clip = clip;
         audioSourceGO[pt.pageIndex - 1].GetComponent<AudioSource>().enabled = false;
         grandManager.GetComponent<AudioSource>().enabled = false;
@@ -64,10 +64,9 @@ public class MenuManager : MonoBehaviour {
     //    // Screen.SetResolution(720,720,false);
     //}
 
-    public void PlayDIYVideo() {
+    public void PlayDIYVideo(VideoClip clip) {
         StopMusic();
-        videoPlayer.clip = DIYClips[pt.pageIndex - 1];
-
+        vp.clip = clip;
         // Toggle pagebuttons and bcg music off!
         grandManager.GetComponent<AudioSource>().enabled = false;
         audioSourceGO[pt.pageIndex - 1].GetComponent<AudioSource>().enabled = false;
@@ -100,22 +99,32 @@ public class MenuManager : MonoBehaviour {
         audioData.clip = musicClips[pt.pageIndex - 1];
         grandManager.GetComponent<AudioSource>().enabled = false;
         audioSourceGO[pt.pageIndex - 1].GetComponent<AudioSource>().enabled = false;
+        ToggleParticles(musicButtonParticles[pt.pageIndex - 1].gameObject);
         if (audioData.isPlaying) {
             audioData.Stop();
             grandManager.GetComponent<AudioSource>().enabled = true;
             audioSourceGO[pt.pageIndex - 1].GetComponent<AudioSource>().enabled = true;
-
+            //ToggleParticles(musicButtonParticles[pt.pageIndex]);
         } else if (!audioData.isPlaying)
             audioData.Play();
     }
 
     public void StopMusic() {
+        
         if (audioData.isPlaying) {
             audioData.Stop();
             grandManager.GetComponent<AudioSource>().enabled = true;
             audioSourceGO[pt.pageIndex - 1].GetComponent<AudioSource>().enabled = true;
-
         }
+        if (pt.pageIndex > 0) {
+            if (musicButtonParticles[pt.pageIndex - 1].gameObject.activeSelf) {
+                musicButtonParticles[pt.pageIndex - 1].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void ToggleParticles(GameObject ps) {
+        ps.SetActive(!ps.activeSelf);
     }
 
     public void Credits() {
