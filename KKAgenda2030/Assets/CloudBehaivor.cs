513a8c7f9cce75a40e63a4e61a1d21444097cf64
Nikl3 <niko.tuuli@hotmail.com>
2019-06-14 12:02:02 +0300

    climategame menu things... almost ready (Y)

---
+++ KKAgenda2030/Assets/CloudBehaivor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehaivor : MonoBehaviour {
+
+    WeatherToggle wt;
+
+    private void Start() {
+        wt = GameObject.Find("MenuContainer").GetComponent<WeatherToggle>();
+    }
+
+
+    public void FadeClouds() {
+        GetComponent<AnimatorTimer>().enabled = false;
+        GetComponent<Animator>().Play("CloudFade");
+        wt.RemoveCloud();
+    }
+
+}
---
+++ KKAgenda2030/Assets/WeatherToggle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherToggle : MonoBehaviour {
+
+    public int totalClouds;
+    public GameObject rainy;
+    public GameObject sunny;
+    public GameObject rainParticles;
+    public AudioSource audioSource;
+    public AudioClip clip;
+
+
+
+    public void RemoveCloud() {
+        totalClouds--;
+        if (totalClouds == 0) {
+            ChangeWeather();
+        }
+    }
+
+    void ChangeWeather() {
+        rainy.SetActive(false);
+        sunny.SetActive(true);
+        rainParticles.SetActive(false);
+        audioSource.clip = clip;
+        audioSource.volume = 0.4f;
+        audioSource.Play();
+        GetComponent<AudioSource>().Play();
+    }
+
+}
