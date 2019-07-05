e1062d2adc7c1a2c28231352e60635429fabd466
Nikl3_home <nikle34@gmail.com>
2019-07-05 14:23:04 +0300

    missing musics in two games

---
+++ KKAgenda2030/Assets/Scripts/AudioList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioList : MonoBehaviour {
+
+    //this script maybe not needed
+
+    AudioSource audioSource;
+    public List<AudioClip> audioClips;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+}
